a3264852f492c0861c254ed64639785ea347e1c3
Jason-Paprocki <jasonpaprocki@protonmail.com>
2019-11-10 00:45:59 -0500

    erdsag

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ def incoming_sms():
 
     # Determine the right reply for this message
     if 'Start' in body:
-        startInMessage(body)
+        resp.message(startInMessage(body))
     elif 'end' in body:
         resp.message(endInMessage())
 
