8b8ec94edbf8a8585c15d7bc1464f27d6d646bb3
Jason-Paprocki <jasonpaprocki@protonmail.com>
2019-11-10 09:02:50 -0500

    asdf

--- app.py
+++ app.py
@@ -47,10 +47,10 @@ def incoming_sms():
     return str(resp)
 
 def findPhoneNumber():
-    with open('mycsv.csv') as csv_file:
+    with open('mycsv.csv', 'r') as csv_file:
         csv_reader = csv.reader('mycsv.csv')
         csv_list = list(csv_reader)
-        print(list)
+        print(csv_list)
 
 if __name__ == "__main__":
     app.run(debug=True)
