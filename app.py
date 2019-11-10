52f36b79acb7b1b4a503d877994e530ab10809dc
Jason-Paprocki <jasonpaprocki@protonmail.com>
2019-11-10 00:24:53 -0500

    adfsg

--- app.py
+++ app.py
@@ -45,9 +45,9 @@ def endInMessage():
 def phoneNumberHandler(phone_number):
     rows = list(csv_f)
 
-    for row in rows:
-        if (str(rows[row][0]) == str(phone_number)):
-            print(row)
+    for i in range(rows):
+        if (str(rows[i][0]) == str(phone_number)):
+            print(i)
             print("beep")
 if __name__ == "__main__":
     app.run(debug=True)
