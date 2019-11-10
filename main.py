97168ea6e5a51fb7746a449790626a40dd524e94
Ricky Palaguachi <rickyp2413@gmail.com>
2019-11-10 05:54:14 -0500

    driver code

--- main.py
+++ main.py
@@ -2,37 +2,11 @@ import json
 import requests
 
 key = "AIzaSyB3O7rrXNzMCbD1dK3Kmme_yCx3PruCVwk"
-
-#parsejson(filename of whatever json data google sends us to route from A to B)
-#gather list of (lat, lng) coordinate pairs of turns along that route
-#At every midpoint of two coordinate pairs, we find best gas prices using gasbuddy webscraper
-#store that data in a list
-#Compare the best prices along the route, and display the top choices
-
-
-if __name__ == '__main__':
-    route_AtoB_json = get_route(gmap, origin, destination)
-    pointA = data_from_route[0][0]
-    pointB = data_from_route[0][1]
-    turns_list = parse_json_for_turns(route_AtoB_json)[1]
-
-    midpoint_list = []
-    for i in range(len(turns_list)):
-        if i+1 < len(turns_list):
-            midpoint = turns_list[i].eucl_midpoint_between(turns_list[i+1])
-            midpoint_list.append(midpoint)
-
-    for midpoint in midpoint_list:
-        address = midpoint.to_address()
-
-    
-
-
-import googlemaps
-import json
-
 gmap = googlemaps.Client(key='AIzaSyB3O7rrXNzMCbD1dK3Kmme_yCx3PruCVwk')
+origin = "20 John St, Little Ferry, NJ"
+destination = "250 Central Ave, Newark, NJ"
 
+#Gets Google Map route from Origin to Destination
 def get_route(gmap, origin, destination):
     result = gmap.directions(origin, destination)
     return result
@@ -40,4 +14,64 @@ def get_route(gmap, origin, destination):
 def jsonify(request_info):
     with open('json_dump.json', 'w') as ofile:
         json.dump(request_info, ofile)
-jsonify(getRequest(gmap, '42 wilsey street, newark, nj', 'test_address'))
+
+#Sorts Gas Station objects by their price
+def sort_by_price(gs_list):
+    #in place insertion sort
+    for gs in gs_list:
+        # sort in place
+        for i in range(1, len(gs_list)):
+            current = gs_list[i]
+
+            while i > 0 and gs_list[i-1] > current:
+                gs_list[i] = my_list[i-1]
+                i -= 1
+
+            gs_list[i] = current
+
+#generates a GoogleMaps url to nearest gas Station
+#in close proximity of original route from A to B
+def generate_final_route(pointA, pointB, gs):
+    url = "https://www.google.com/maps/dir/?"
+    payload = {"api" : 1,
+               "origin" : pointA,
+               "destination" : pointB ,
+               "waypoints" : gs}
+
+    final_url = url + "api=1"
+    final_url += "origin=" : pointA.get_lat_lng_str()
+    final_url += "destination=" : pointB.get_lat_lng_str()
+    final_url += "waypoints=" : gs.point.get_lat_lng_str()
+
+    return final_url
+
+def main(origin, destination):
+    #generate a json file to grab turn data from route of origin->destination
+    route_AtoB_json = get_route(gmap, origin, destination)
+    pointA = data_from_route[0][0]
+    pointB = data_from_route[0][1]
+    turns_list = parse_json_for_turns(route_AtoB_json)[1]
+
+    #calculate the midpoint of all consecutive turns and list them
+    midpoint_list = []
+    for i in range(len(turns_list)):
+        if i+1 < len(turns_list):
+            midpoint = turns_list[i].eucl_midpoint_between(turns_list[i+1])
+            midpoint_list.append(midpoint)
+
+    #determine best gas stations in proximity to to each midpoint
+    gas_stations = []
+    for midpoint in midpoint_list:
+        address = midpoint.to_address()
+        gas_stations.append(get_station_at_address(address))
+
+    #gather final google maps url of pointA->gas_stations[0]->pointB
+    gas_stations = sort_by_price(gas_stations)
+    route_url = generate_final_routes(pointA, pointB, gas_stations[0])
+
+    print(route_url)
+
+if __name__ == '__main__':
+    main(origin, destination)
+
+jsonify(get_route(gmap, '42 wilsey street, newark, nj', 'test_address'))
