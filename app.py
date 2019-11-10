4fb0371e48c60b87ba79dd4dfc10934a4f8b5172
Jason-Paprocki <jasonpaprocki@protonmail.com>
2019-11-10 00:20:02 -0500

    asdfgdfa

--- app.py
+++ app.py
@@ -50,8 +50,8 @@ def phoneNumberHandler(phone_number):
     print(str(rows[1][0]) == str(phone_number))
 
     for row in csv_f:
-        if str(phone_number) in str(rows[1][0]):
-            print("yayay")
+        if (str(rows[1][0]) == str(phone_number)):
+            print("why")
 
 if __name__ == "__main__":
     app.run(debug=True)
