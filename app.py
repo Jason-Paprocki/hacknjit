d43cc1eaf9ce1070c7249713f5b664ea7cc1fb73
jasonpaprocki <jasonpaprocki@protonmail.com>
2019-11-09 20:40:13 -0500

    nive

--- app.py
+++ app.py
@@ -5,19 +5,23 @@ app = Flask(__name__)
 
 @app.route("/sms", methods=['GET', 'POST'])
 def incoming_sms():
+    # Start our TwiML response
+    resp = MessagingResponse()
+
     """Send a dynamic reply to an incoming text message"""
     # Get the message the user sent our Twilio number
     body = request.values.get('Body', None)
 
-    # Start our TwiML response
-    resp = MessagingResponse()
+    #get the cookie value, or default to zero
+    messagecount = int(request.cookies.get('messagecount',0))
+    messagecount += 1
 
-    # Determine the right reply for this message
-    if body == 'hello':
-        body_again = request.values.get('Body', None)
-        resp.message(body + body_again)
-    elif body == 'bye':
-        resp.message("Goodbye")
+    resp.message("You've sent " + str(messagecount) + " messages in this conversation so far")
+
+
+    expires=datetime.utcnow() + timedelta(hours=4)
+    resp.set_cookie('messagecount',value=str(messagecount),expires=expires.strftime('%a, %d %b %Y %H:%M:%S GMT'))
+
 
     return str(resp)
 
