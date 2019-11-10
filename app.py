65806c534b40f1f085f2b24810ef1724257f9618
Jason-Paprocki <jasonpaprocki@protonmail.com>
2019-11-10 00:40:36 -0500

    eswhgt

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ def incoming_sms():
 
     return str(resp)
 
-
+rows = list(csv_f)
 
 def startInMessage():
     return "testing works"
@@ -43,12 +43,11 @@ def endInMessage():
     return "yert"
 
 def phoneNumberHandler(phone_number):
-    rows = list(csv_f)
 
     for i in range(len(rows)):
         if (str(rows[i][0]) == str(phone_number)):
             print(i)
             print("beep")
-            
+
 if __name__ == "__main__":
     app.run(debug=True)
