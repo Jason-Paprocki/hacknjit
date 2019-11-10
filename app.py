378b7411d0940deeedf0c78e999d06ecf7912ffc
jasonpaprocki <jasonpaprocki@protonmail.com>
2019-11-09 20:14:55 -0500

    minasdgda

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import json
 from flask import Flask, request, redirect
+from datetime import datetime, timedelta
 from twilio.twiml.messaging_response import MessagingResponse
 
 app = Flask(__name__)
@@ -10,24 +11,41 @@ def incoming_sms():
     # Get the message the user sent our Twilio number
     body = request.values.get('Body', None)
 
-    # Start our TwiML response
-    resp = MessagingResponse()
+
+    #get the cookie value, or default to zero
+    messagecount = int(request.cookies.get('messagecount',0))
+    messagecount += 1
+
+    twml = twiml.Response()
+    twml.message("You've sent " + str(messagecount) + " messages in this conversation so far")
+
+    resp = make_response(str(twml))
+
+    expires=datetime.utcnow() + timedelta(hours=4)
+    resp.set_cookie('messagecount',value=str(messagecount),expires=expires.strftime('%a, %d %b %Y %H:%M:%S GMT'))
+
+
+'''
 
     parsed_json = json.loads(body)
     json.dump(parsed_json, indent=4)
 
-    """
-    End goal JSON file:
+
+    message_data =
     {
         "service": "gas"
         "start":
         "end":
     }
-    """
+'''
 
+
+
+
+    # Start our TwiML response
+    resp = MessagingResponse()
     #reply for this message
     resp.message(body)
-
     return str(resp)
 
 if __name__ == "__main__":
